bb60169752c21676d3b20cfc2daadfbb7b4f1d2b
saganic <chris@saganic.co.uk>
2024-10-30 10:12:35 +0000

    initial commit

---
+++ app.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+export interface User {
+  name: string
+  email: string
+  phone: string
+}
+
+const user1 = ref<User>(initUser())
+
+const user2 = ref<User>(initUser())
+
+function initUser() {
+  return {
+    name: '',
+    email: '',
+    phone: '',
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div style="border: 1px solid grey; padding: 4px">
+      <p>User 1</p>
+      <UserForm
+        v-model="user1"
+      />
+      {{ user1 }}
+    </div>
+
+    <div style="border: 1px solid grey; padding: 4px">
+      <p>User 2</p>
+      <UserForm
+        v-model="user2"
+      />
+      {{ user2 }}
+    </div>
+
+    <div style="border: 1px solid grey; padding: 4px">
+      <button @click="user1 = initUser()">
+        Reset user1
+      </button>
+      <button @click="user2 = initUser()">
+        Reset user2
+      </button>
+    </div>
+  </div>
+</template>
---
+++ components/UserForm.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import type { User } from '~/app.vue'
+
+const emit = defineEmits<{
+  (e: 'submit', value: User): void
+}>()
+
+const model = defineModel<User>({
+  default: {
+    name: '',
+    email: '',
+    phone: '',
+  },
+})
+</script>
+
+<template>
+  <form @submit.prevent="emit('submit', model)">
+    <div
+      class="form-group"
+    >
+      <label for="name">Name</label>
+      <input id="name" v-model="model.name" type="text">
+    </div>
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input id="email" :value="model.email" type="email">
+    </div>
+    <div class="form-group">
+      <label for="phone">Phone</label>
+      <input id="phone" :value="model.phone" type="text">
+    </div>
+    <button type="submit">
+      Submit
+    </button>
+  </form>
+</template>
